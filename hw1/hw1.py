1949a41f10600e90a3e537d69deb07896b0f4c6a
Rohan <rohanvirani@hotmail.co.uk>
2021-10-04 14:29:22 -0700

    hello

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -138,7 +138,7 @@ def main(config):
     # Create model and optimizer
     model = MANN(config.num_classes, config.num_samples, 
                  model_size=config.model_size)
-    
+    print(device)
     model = model.to(device)
     optim = torch.optim.Adam(model.parameters(), lr = 1e-3)
     
